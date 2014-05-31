9ce8edfd66ea2aee8a1ded429c614cd215b0cae3
Alex Birdsall <ambirdsall@gmail.com>
2014-05-30 20:58:40 -0700

    adds pseudocode, burglar, path, sneak, and turns

--- week_3/5_design_cc_mission_solo_challenge/my_solution.js
+++ week_3/5_design_cc_mission_solo_challenge/my_solution.js
@@ -1,12 +1,14 @@
 // This is a solo challenge
 
 // Your mission description:
-// 
-// 
-// 
+// CAT BURGLAR! Steal the jewels by night. That's why there's no graphics. Because it's dark.
+// You drop through a window into a jewelry store. That's where the game opens. You know one safe path past the motion detectors.
+// You have to sneak 10' forwards, turn left, sneak 10' forwards, turn right, sneak 5' forwards, snatch the jewels. Luckily, you've
+// trained long and hard to sneak exactly 5' at a time. Then you retrace your steps (turn either direction x2 first, natch) and climb 
+// back out the window.
 
 // Pseudocode
-// 
+// burglar object with location (x,y) and methods: sneak forwards in increments of 5', turn left, turn right, snatch jewels, climb out
 // 
 // 
 // 
