10ad7b29f6c93376fa7c45b95bd990d7317834be
Alex Birdsall <ambirdsall@gmail.com>
2014-06-03 19:25:17 -0700

    adds reflection

--- week_3/6_group_project/my_solution.js
+++ week_3/6_group_project/my_solution.js
@@ -1,3 +1,34 @@
 // My role in the group is:
 
 // Here is my part of the challenge:
+
+
+
+// Reflection:
+// I was super excited reading the premise of the group project, although I really wanted to be involved in the actual coding.
+// Instead, I had the role of creating the user stories and evaluating the refactored code vis. the tests. It wasn't altogether
+// boring: I really enjoy reading code directly, so figuring out the necessary tasks from the test code was fun. Nonetheless,
+// I felt a bit left out. I sent out my user stories and, some days later, was given a set of tidy code that was most of the
+// way to being what we needed. While I saw what my user stories got across and what they didn't, the feeling was less "aha" 
+// than "oh, right".
+
+// Our group wasn't successful in passing most of the tests. Tests number 1-6 foundered on the fact that our code for sum and 
+// mean weren't functions. The logic of our operations was perfect, and the code was clean and legible: it would have been a 
+// really nice solution had it been inside a function's code block. But if wishes were horses, we'd all be eating steak. Unlike
+// the other two operations, median WAS a function. When tests 1-6 were commented out, we passed 7-9 with a clean slate.
+
+// It was a pretty good object lesson for me about writing user stories: you can't be too specific. It was a little hard having
+// to work around the technical language; in retrospect, it would have made life easier (or the tests more successful, in any
+// case) had I put in something like "these actions should be modular, so that I can use the same product on any set of numbers
+// I want". Even rephrasing it here, I found it hard not to use the word "functions" instead of "actions". Even though the word
+// has a technical meaning, it also has a common language meaning that fits nicely in context. I definitely struggled with that
+// ethically, and I'm not sure where precisely the line lies. Still, going more in-depth with my user stories would have let me
+// paint a more vivid picture indicating implicitly some structural matters for the final code, as well as its arithmetical
+// functionalities.
+
+
+
+
+// - What was this experience like? 
+// - Was your group successful in passing the tests? 
+// - What did you learn about writing user stories, pseudocoding, writing code, refactoring, etc.?
